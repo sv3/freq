01d97bf3ac5b1b8f8d625a15c6f03be521b9efce
Simon Vana <simon.vana@gmail.com>
2016-11-16 11:43:00 +0000

    combine py files, add interrupt to signal new data

--- rpm.p
+++ rpm.p
@@ -1,5 +1,7 @@
 .origin 0
 
+#define PRU0_ARM_INTERRUPT 19
+
 #define CONST_PRUCFG         C4
 #define CONST_PRUSHAREDRAM   C28
  
@@ -13,26 +15,26 @@
 #define SHARED_RAM           0x100
 
 #define SECOND               200000000      // 2e8 5ns cycles
+#define DELAY                  2000000      // 0.01 s
     
 START:
-    LBCO    r0, CONST_PRUCFG, 4, 4          // Enable OCP master port
-    CLR     r0, r0, 4
-    SBCO    r0, CONST_PRUCFG, 4, 4
+    lbco r0, CONST_PRUCFG, 4, 4          // Enable OCP master port
+    clr  r0, r0, 4
+    sbco r0, CONST_PRUCFG, 4, 4
     
-    MOV     r0, SHARED_RAM                  // Set C28 to point to shared RAM
-    MOV     r1, PRU0_CTRL + CTPPR0
-    SBBO    r0, r1, 0, 4
+    mov  r0, SHARED_RAM                  // Set C28 to point to shared RAM
+    mov  r1, PRU0_CTRL + CTPPR0
+    sbbo r0, r1, 0, 4
 
-    mov  r2.w0, SECOND & 0xFFFF
-    mov  r2.w2, SECOND >> 16 
+    mov  r2.w0, DELAY & 0xFFFF
+    mov  r2.w2, DELAY >> 16 
 
-CNT:
-    mov  r0, 0
+WAITLO:
+    qbbc WAITLO, r31.t15
 
-DEBOUNCE:                              // Wait for 50 consecutive readings of 1
-    add  r0, r0, 2                      // 3 cycles per loop
-    //qbbc CNT, r31.t15                // Reset if input goes to 0
-    qbgt DEBOUNCE, r0, r2
+IGNORE:                              // Wait for 50 consecutive readings of 1
+    add  r0, r0, 2                     // 3 cycles per loop
+    qbgt IGNORE, r0, r2
 
 WAIT1:                                 // Continue counting while input is still 1
     add  r0, r0, 2
@@ -43,4 +45,6 @@ WAIT2:                                 // Continue counting while input is 0
     qbbc WAIT2, r31.t15                // End count when input goes to 1
 
     sbco r0, CONST_PRUSHAREDRAM, 0, 4  // Write count to RAM
-    jmp  CNT
+    MOV R31.b0, PRU0_ARM_INTERRUPT+16   // Send notification to Host for program completion
+    mov  r0, 0
+    jmp  IGNORE
--- rpm.py
+++ rpm.py
@@ -1,16 +1,46 @@
 # rpm.py - gets rpm value
 
+from __future__ import print_function
+
 import pypruss
 import os
 
-os.system("config-pin p8.12 pruout")
+import mmap
+import struct
+from time import sleep
+
+PRU_ICSS = 0x4A300000
+START = 0x00010000
+
+def readrpm():
+    with open('/dev/mem', 'r+b') as m:
+        mem = mmap.mmap( m.fileno(), 512*1024, offset=PRU_ICSS)
+
+        valuestring = mem[START:START+4]
+        value = struct.unpack('L', valuestring)[0]
+        seconds = value * 0.000000005   # 5ns per cycle
+
+        return seconds
+
+
+os.system("config-pin p8.15 pruin")
 
 pypruss.modprobe()                                  # This only has to be called once pr boot
 pypruss.init()                                      # Init the PRU
 pypruss.open(0)                                     # Open PRU event 0 which is PRU0_ARM_INTERRUPT
-#pypruss.pruintc_init()                              # Init the interrupt controller
-pypruss.exec_program(0, "./rpm.bin")           # Load firmware "blinkled.bin" on PRU 0
-#pypruss.wait_for_event(0)                           # Wait for event 0 which is connected to PRU0_ARM_INTERRUPT
-#pypruss.clear_event(0,pypruss.PRU0_ARM_INTERRUPT)   # Clear the event
-#pypruss.pru_disable(0)                              # Disable PRU 0, this is already done by the firmware
-#pypruss.exit()                                      # Exit, don't know what this does.
+pypruss.pruintc_init()                              # Init the interrupt controller
+pypruss.exec_program(0, "./rpm.bin")                # Load firmware "blinkled.bin" on PRU 0
+
+try:
+    while True:
+        pypruss.wait_for_event(0)                           # Wait for event 0 which is connected to PRU0_ARM_INTERRUPT
+
+        seconds = readrpm()
+        print( r'{} seconds'.format(seconds) )
+
+        pypruss.clear_event(0,pypruss.PRU0_ARM_INTERRUPT)   # Clear the event
+
+except KeyboardInterrupt as e:
+    print(e)
+    pypruss.pru_disable(0)                             # Disable PRU 0, this is already done by the firmware
+    pypruss.exit()                                     # Exit, don't know what this does.
